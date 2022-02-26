01863f6fbe353f9c03c43286a46e49d68854150c
Erick <ag.erick115@gmail.com>
2022-02-26 14:21:40 -0600

    Time Conversion listo

--- Caso_1_EAG.cpp
+++ Caso_1_EAG.cpp
@@ -41,6 +41,40 @@ void Triplet2(int a[3], int b[3]) { //Esta version de el codigo de tripletas es 
 	cout<<"Triplet2: "<<result[0]<<","<<result[1]<<endl;
 }
 
+void Time1(string time) {
+	string result = time.substr(2, 6);
+	int hours = std::stoi(time.substr(0,2));
+
+	if(time[8]=='A' && hours==12)
+		hours = 0;
+	if(time[8]=='P'){
+		hours += 12;
+	}
+	
+	if(hours<10)
+		cout<<"Time1: "<<"0"+to_string(hours)+result<<endl;
+	else{
+		cout<<"Time1: "<<std::to_string(hours)+result<<endl;
+	}
+}
+
+void Time2(string time) {				//En esta funcion se usa un valor
+	string result = time.substr(2, 6);	//de conversion para aplicarle a las horas
+	int hours = stoi(time.substr(0,2)); //y asi hacer menos comparaciones
+	int convert = (hours%12)/hours;
+
+	if(time[8]=='A')
+		convert--;
+		
+	hours += (12*convert);
+	
+	if(hours<10)
+		cout<<"Time2: "<<"0"+to_string(hours)+result<<endl;
+	else{
+		cout<<"Time2: "<<to_string(hours)+result<<endl;
+	}
+}
+
 int main(){
 
 	int Trip1[]={1,2,3};
@@ -53,6 +87,12 @@ int main(){
 	cout<<endl<<"////////////////////////"<<endl<<endl;
 	Triplet2(Trip1, Trip2);
 	Triplet2(Trip3, Trip4);
+	cout<<endl<<"////////////////////////"<<endl<<endl;
+	Time1("12:43:09AM");
+	Time1("03:21:56PM");
+	cout<<endl<<"////////////////////////"<<endl<<endl;
+	Time2("12:43:09PM");
+	Time2("03:21:56AM");
 
 	return 0;
 }
